c546e91a99781551b7783e1e1bd118d35c92a81f
Gustavo Vinícius Coinete Silva <vinicius.gustavo@estudante.ifmt.edu.br>
2022-10-14 08:34:07 -0300

    Introdução Git

---
+++ src/introdução/Introdução.java
@@ -0,0 +1,11 @@
+
+package introdução;
+
+public class Introdução {
+
+    public static void main(String[] args) {
+        System.out.println("Nosso primeiro projeto com gerenciamento de Versão");
+        System.out.println("Vírusssssssssssssssssssssssssssssssssssssssssssss");
+    }
+    
+}
